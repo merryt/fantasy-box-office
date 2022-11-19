3a633e21fd13bae8342ff67ff6c1101580f40646
merryt <tymerry@gmail.com>
2022-11-19 00:14:45 -0500

    adding list view

--- league/urls.py
+++ league/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='leagues'),
     path('create', views.create, name='create'),
     path('<int:league_id>/', views.details, name='detail'),
 ]
--- league/views.py
+++ league/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse 
 from .models import League
 
 def index(request):
-    context = { "number_of_leauges": League.objects.count()   }
+    context = { "number_of_leagues": League.objects.count(), "list_of_leagues": League.objects.all() }
     return render(request,'league/index.html', context)
 
 def create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+
+        if League.objects.filter(name=name).exists():
+            error = "Name alrady Taken"
+            return render(request,'league/create.html',{'error':error})
+        else:
+
+            league = League.objects.create(name=name)
+            league.save()
+            return redirect('leagues')        
+    
     return render(request,'league/create.html')
 
 
