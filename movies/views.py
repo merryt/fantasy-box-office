366c9a365385540a3634fb5b057ffd8f78f2b302
merryt <tymerry@gmail.com>
2022-11-27 17:23:14 -0500

    don't show upcoming movies that are in the past

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+import datetime
+
 
 def details(request, movie_id):
     current_movie = get_object_or_404(Movie, pk=movie_id)
@@ -7,5 +9,5 @@ def details(request, movie_id):
     return render(request, 'movies/details.html', context)
 
 def index(request):
-    context = { "upcoming_movies": Movie.objects.all().order_by('release_date') }
+    context = { "upcoming_movies": Movie.objects.filter(release_date__gte=datetime.date.today()).order_by('release_date') }
     return render(request,'movies/index.html', context)
