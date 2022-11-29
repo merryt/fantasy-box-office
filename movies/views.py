4399559c010df70947a7ade4f533a4a2d3b37bd7
merryt <tymerry@gmail.com>
2022-11-29 12:06:19 -0500

    implementing ability to remove movies

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Movie
 from users.models import User
 from league.models import Team
@@ -10,15 +10,26 @@ def details(request, movie_id):
     current_movie = get_object_or_404(Movie, pk=movie_id)
     related_teams = Team.objects.filter(player=request.user.id)
     only_one_team = related_teams.count() == 1
+    is_movie_on_team = False
+    
+    if(only_one_team):
+        team = related_teams.first()
+        is_movie_on_team = team.movie_picks.filter(id=movie_id).count() >= 1
 
     # todo: build "select for team" logic
-    # todo: figure out if this movie is already on that players team
+    
     if request.method == 'POST':
-        if(only_one_team):
-            related_teams.first().movie_picks.add(current_movie)
-            related_teams.first().save()
-            print(related_teams.first().movie_picks)
-    context = { "movie" : current_movie, "only_one_team": only_one_team }
+        if 'remove_movie' in request.POST:
+            team.movie_picks.remove(current_movie)
+            team.save()
+            is_movie_on_team = False
+        
+        elif 'add_movie' in request.POST:
+            team.movie_picks.add(current_movie)
+            team.save()
+            is_movie_on_team = True  
+            
+    context = { "movie" : current_movie, "only_one_team": only_one_team, "is_movie_on_team":is_movie_on_team, }
     return render(request, 'movies/details.html', context)
 
 def index(request):
